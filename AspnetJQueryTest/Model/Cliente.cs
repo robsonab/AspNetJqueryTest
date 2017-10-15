9cfed2abb4a872aa84dd61fc3d1f09623c14ef4a
Robson Barbosa <robson.a.barbosa@outlook.com>
2017-10-15 17:31:31 -0200

    Novo projeto

---
+++ AspnetJQueryTest/Model/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetJQueryTest.Model
+{
+    public class Cliente
+    {
+        private int codigo;
+
+        public int Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        private string nome;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        private string telefone;
+
+        public string Telefone
+        {
+            get { return telefone; }
+            set { telefone = value; }
+        }
+    }
+}
---
+++ AspnetJQueryTest/Model/Context.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetJQueryTest.Model
+{
+    public class Context
+    {
+        private static List<Cliente> clientes;
+
+        public static List<Cliente> Clientes
+        {
+            get
+            {
+                if (clientes == null)
+                    IniciarClientes();
+                return Context.clientes;
+            }
+            set { Context.clientes = value; }
+        }
+
+        private static void IniciarClientes()
+        {
+            clientes = new List<Cliente>();
+            clientes.Add(new Cliente() { Codigo = 1, Nome = "Cliente 001", Telefone = "3322-1144" });
+            clientes.Add(new Cliente() { Codigo = 2, Nome = "Cliente 002", Telefone = "1234-5678" });
+            clientes.Add(new Cliente() { Codigo = 3, Nome = "Cliente 003", Telefone = "9988-7766" });
+        }
+    }
+}
